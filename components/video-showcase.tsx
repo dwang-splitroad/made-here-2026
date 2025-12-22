e91ad23c15f495599624b19d49e20c0ba972ddb4
Dennis <dennis@splitroadmedia.com>
2025-12-22 00:22:19 -0500

    added new main video

--- components/video-showcase.tsx
+++ components/video-showcase.tsx
@@ -8,11 +8,11 @@ export default function VideoShowcase() {
 
       <div className="relative aspect-video bg-card border border-border overflow-hidden">
         <iframe
-          src="https://player.vimeo.com/video/1140137345?badge=0&autopause=0&player_id=0&app_id=58479"
+          src="https://player.vimeo.com/video/1148444849?title=0&byline=0&portrait=0&badge=0&autopause=0&player_id=0&app_id=58479"
           className="absolute top-0 left-0 w-full h-full"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
           style={{ border: 0 }}
-          title="Made Here | 2025"
+          title="Made Here 2025 Intro Video"
         />
       </div>
     </section>
